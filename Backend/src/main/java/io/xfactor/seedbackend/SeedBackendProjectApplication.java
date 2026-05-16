e8a9eeb8b05317ab62cf1b5467b37a12b950eaf3
Luis E. Andonaire Ezeta <LUIS.ANDONAIRE.EZ@GMAIL.COM>
2026-05-16 00:11:56 -0500

    Cambio mínimo: agregar comentario en main

--- Backend/src/main/java/io/xfactor/seedbackend/SeedBackendProjectApplication.java
+++ Backend/src/main/java/io/xfactor/seedbackend/SeedBackendProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeedBackendProjectApplication {
 
+//PRUEBA
     public static void main(String[] args) {
         SpringApplication.run(SeedBackendProjectApplication.class, args);
     }
